8636eb590df1dee877eb0a4981b686f40e7e5470
adhoprsy <adhoprsy@gmail.com>
2024-11-21 10:47:40 +0800

    keymap shorter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,20 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local set = vim.keymap.set
+local del = vim.keymap.del
+
 -- ctrl + q exit nvim
-vim.keymap.set("n", "<C-q>", ":qa<CR>", { noremap = true, silent = true })
+set("n", "<C-q>", ":qa<CR>", { noremap = true, silent = true })
 
 -- f7 toggle terminal
-vim.keymap.set("n", "<F7>", function()
-    Snacks.terminal()
+set("n", "<F7>", function()
+  Snacks.terminal()
 end, { desc = "Termnal (cwd)" })
 
-vim.keymap.set("t", "<F7>", "<cmd>close<cr>", { desc = "Hide Termnal" })
+set("t", "<F7>", "<cmd>close<cr>", { desc = "Hide Termnal" })
 
-vim.keymap.del("n", "<c-/>")
-vim.keymap.del("n", "<c-_>")
-vim.keymap.del("t", "<C-/>")
-vim.keymap.del("t", "<c-_>")
+del("n", "<c-/>")
+del("n", "<c-_>")
+del("t", "<C-/>")
+del("t", "<c-_>")
 
 -- comment
-vim.keymap.set({ "n", "i", "v" }, "<c-/>", "<cmd>normal gcc<cr>", { desc = "toggle comment" })
+set({ "n", "i", "v" }, "<c-/>", "<cmd>normal gcc<cr>", { desc = "toggle comment" })
