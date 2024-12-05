ad97ae52c173fbcd7d287aac9783e35b1e41ae33
adhoprsy <adhoprsy@gmail.com>
2024-12-05 09:54:34 +0800

    theme everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,24 +4,24 @@ return {
     lazy = true,
     name = "catppuccin",
     opts = {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      flavour = "macchiato",          -- latte, frappe, macchiato, mocha
       transparent_background = false, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+      term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
       dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
+        enabled = false,              -- dims the background color of inactive window
         shade = "dark",
-        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        percentage = 0.15,            -- percentage of the shade to apply to the inactive window
       },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
+      no_italic = false,              -- Force no italic
+      no_bold = false,                -- Force no bold
+      no_underline = false,           -- Force no underline
+      styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" },      -- Change the style of comments
         conditionals = { "italic" },
         loops = {},
         functions = {},
-        keywords = {},
+        keywords = { "bold" },
         strings = {},
         variables = {},
         numbers = {},
@@ -90,31 +90,32 @@ return {
   },
   {
     "EdenEast/nightfox.nvim",
-    lazy = true,
-    opts = {
-      -- Compiled file's destination location
-      compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-      compile_file_suffix = "_compiled", -- Compiled file suffix
-      transparent = false, -- Disable setting background
-      terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-      dim_inactive = false, -- Non focused panes set to alternative background
-      module_default = true, -- Default enable value for modules
-      styles = { -- Style to be applied to different syntax groups
-        comments = "italic", -- Value is any valid attr-list value `:help attr-list`
-        keywords = "italic",
-      },
-    },
-    palettes = {
-      all = {
-        bg1 = "#292f3a",
-      },
-    },
+    lazy = false,
+    config = function()
+      require('nightfox').setup({
+        options = {
+        }
+      })
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        background = "medium",
+        italics = true,
+      })
+    end,
   },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nordfox",
+      colorscheme = "everforest",
     },
   },
 }
